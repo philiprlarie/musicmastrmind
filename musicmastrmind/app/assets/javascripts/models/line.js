140f95230b4856d244eb8e98f1a714a14dfca1a5
philiprlarie <philiprlarie@gmail.com>
2015-08-14 17:24:41 -0700

    Add parsing functionality for lines with thier interpretations parsed out

--- musicmastrmind/app/assets/javascripts/models/line.js
+++ musicmastrmind/app/assets/javascripts/models/line.js
@@ -1,3 +1,21 @@
 MusicMastrMind.Models.Line = Backbone.Model.extend({
-  urlRoot: "/api/lines"
+  urlRoot: "/api/lines",
+
+  parse: function (response) {
+    if (response.interpretations) {
+      // only need to parse if we will be nesting things inside interpretations. we might need to parse inside. either way, doesn't hurt.
+      this.interpretations().set(response.interpretations, { parse: true });
+      delete response.interpretations;
+    }
+
+    return response;
+  },
+
+  interpretations: function () {
+    if (!this._interpretations) {
+      this._interpretations = new MusicMastrMind.Collections.Interpretations([], { line: this });
+    }
+
+    return this._interpretations;
+  }
 });
