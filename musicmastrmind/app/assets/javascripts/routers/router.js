29f4dd55762d6d0390f9d459f9baa7fa9e5a0efb
philiprlarie <philiprlarie@gmail.com>
2015-08-19 17:47:50 -0700

    Artist Show page. links to the artist's songs

--- musicmastrmind/app/assets/javascripts/routers/router.js
+++ musicmastrmind/app/assets/javascripts/routers/router.js
@@ -2,6 +2,7 @@ MusicMastrMind.Routers.Router = Backbone.Router.extend({
   initialize: function (options) {
     this.$rootEl = options.$rootEl;
     this.songs = new MusicMastrMind.Collections.Songs();
+    this.artists = new MusicMastrMind.Collections.Artists();
   },
 
   routes: {
@@ -9,7 +10,8 @@ MusicMastrMind.Routers.Router = Backbone.Router.extend({
     "songs/index": "SongsIndex", // TODO doesn't really belong. for developement
     "songs/:id": "SongShow",
     "artists/index/all": "ArtistsIndex",
-    "artists/index/:letter": "ArtistsIndexByLetter"
+    "artists/index/:letter": "ArtistsIndexByLetter",
+    "artists/:id": "ArtistShow"
   },
 
   LandingPage: function () {
@@ -37,11 +39,20 @@ MusicMastrMind.Routers.Router = Backbone.Router.extend({
   },
 
   ArtistsIndexByLetter: function (letter) {
+    // usually will be search by letter, but could be more than one letter.
+    // in that case it will find a match from the beginning of the name
     var artists = new MusicMastrMind.Collections.Artists();
     artists.fetch({ data: { letter: letter } });
     var view = new MusicMastrMind.Views.ArtistsIndex({ collection: artists });
     this._swapView(view);
   },
+
+  ArtistShow: function (id) {
+    var artist = this.artists.getAndFetch(id);
+    var view = new MusicMastrMind.Views.ArtistShow({ model: artist });
+    this._swapView(view);
+  },
+
 
 
   _swapView: function (view) {
---
+++ musicmastrmind/app/assets/javascripts/views/artists/artist_show.js
@@ -0,0 +1,35 @@
+MusicMastrMind.Views.ArtistShow = Backbone.CompositeView.extend({
+  template: JST['artists/show'],
+
+  className: 'artistShow group',
+
+  initialize: function () {
+    this.listenTo(this.model, "sync add remove", this.render);
+    this.listenTo(this.model.songs(), "sync", this.addSongsIndex);
+
+    this.addSubviews();
+  },
+
+  addSubviews: function () {
+    this.addSongsIndex();
+  },
+
+  addSongsIndex: function (interpretation) {
+    this.removeSubviews('.artist-songs');
+    var songsIndex =
+      new MusicMastrMind.Views.SongsIndex({
+        collection: this.model.songs()
+      });
+    this.addSubview(".artist-songs", songsIndex);
+  },
+
+  render:  function () {
+    var view = this;
+    var content = this.template({
+      artist: this.model,
+    });
+    this.$el.html(content);
+    this.attachSubviews(); // subviews $els get put into the dom. subviews are already rendered
+    return this;
+  }
+});
